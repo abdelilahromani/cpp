21d4ad5e01ba33dd87faeb1f2c3a0945fba4befb
Abdelilah Romani <aromani@e1r3p2.1337.ma>
2025-11-09 17:58:46 +0100

    ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aromani <aromani@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/09 17:35:12 by aromani           #+#    #+#             */
+/*   Updated: 2025/11/09 17:56:21 by aromani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int ac, char *av[])
+{
+    int i;
+    int j;
+
+    j = 0;
+    i = 1;
+    if (ac == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
+    }
+    while (i < ac)
+    {
+        j = 0;
+        while (av[i][j])
+        {
+            av[i][j] = toupper(av[i][j]);
+            std::cout << av[i][j];
+            j++;
+        }
+        i++;
+    }
+    std::cout << std::endl;
+}
+
